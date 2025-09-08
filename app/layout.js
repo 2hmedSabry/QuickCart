757fd8ad05bd41266091015ee4c7a94c18f66119
2hmedsabry <ahmed.s.elsshourbagy@gmail.com>
2025-09-09 01:16:15 +0300

    fixing error

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,17 @@
 import { Outfit } from "next/font/google";
 import "./globals.css";
-import { AppContextProvider } from "@/context/AppContext";
-import { Toaster } from "react-hot-toast";
-import { ClerkProvider, SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
-const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { AppContextProvider } from "@/context/AppContext";
+
+const outfit = Outfit({ subsets: ["latin"], weight: ["300", "400", "500"] });
 
 export const metadata = {
   title: "QuickCart - GreatStack",
@@ -13,25 +20,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-<ClerkProvider>
-      <html lang="en">
-        <body className={`antialiased`}>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
+    
+    <ClerkProvider>
+          <AppContextProvider>
+    
+    <html lang="en">
+      <body className={`antialiased`}>
           {children}
-        </body>
-      </html>
-    </ClerkProvider>
+      </body>
+    </html>
+                </AppContextProvider>
+        </ClerkProvider>
+
   );
 }
